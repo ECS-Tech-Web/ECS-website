9e7124dc3ec8df5d40de9a4155b36b8187638075
Nikk6174 <nikk.6174@gmail.com>
2025-01-16 13:40:19 +0530

    text centre

--- Frontend-ECS/src/pages/home/Home.jsx
+++ Frontend-ECS/src/pages/home/Home.jsx
@@ -6,35 +6,39 @@ import Annual from './Annual';
 import ParticlesComponent from '../Particle/Particle';
 import Typewriter from "typewriter-effect";
 
+
 export default function Home() {
     return (
         <div className="mx-auto w-full space-y-10 overflow-x-hidden">
             <ParticlesComponent id='particlejs' />
-            <div  className='absolute w-full top-1/3'>
-                    <h2 className="text-white flex justify-center items-center text-4xl font-bold sm:text-5xl pl-6">
-                        Electronics and Communication Society
-                    </h2>
-                    <div id="hero" className="text-yellow-400 flex justify-center items-center text-4xl font-bold sm:text-5xl pl-6">
-              <span className='typingText'>
-              <Typewriter
-                onInit={(typewriter) => {
-                    typewriter
-                        .typeString("NIT SILCHAR")
-                        .pauseFor(1000)
-                        .start();
-                }}
-            />
-                </span>
-            </div>
-                    
+            <div className='absolute w-full top-1/3 flex flex-col'>
+            
+           
+                <h2 className=" title text-white flex justify-center items-center text-center text-4xl font-bold sm:text-5xl ">
+                    Electronics and Communication Society
+                </h2>
+               
+                <div id="hero" className="text-yellow-400 flex justify-center items-center text-4xl font-bold sm:text-5xl pl-6">
+                    <span className='typingText'>
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .typeString("NIT SILCHAR")
+                                    .pauseFor(1000)
+                                    .start();
+                            }}
+                        />
+                    </span>
                 </div>
-                <div className="pc:h-[90vh] mobile:h-[90vh]" ></div>
-            <Message/>
-            <AboutUs/>
-            <Annual/>
-            <Gallery/>
+
+            </div>
+            <div className="pc:h-[90vh] mobile:h-[90vh]" ></div>
+            <Message />
+            <AboutUs />
+            <Annual />
+            <Gallery />
         </div>
-        
+
     );
-    
+
 }
