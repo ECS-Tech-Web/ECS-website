7a52a0c9d256bdebadfdfec2ccb6cca9a5750244
tri-dip <kalitaminu464@gmail.com>
2025-09-03 12:07:43 +0530

    final changes

--- Frontend-ECS/src/components/Footer.jsx
+++ Frontend-ECS/src/components/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
         <div className="text-center tablet:text-left mb-6 tablet:mb-0 tablet:ml-[5vw] xl:ml-0">
           <h2 className="text-foot text-base mb-3">QUICK LINKS</h2>
           <ul className="mobile:-translate-x-[20vw] tablet:transform-none top-0 space-y-2 text-base text-left font-rajdhani">
-            <li><a href="/Resource+Hub" className="hover:text-[#819BE2]">Resource Hub</a></li>
-            <li><a href="" className="hover:text-[#819BE2]">Messages</a></li>
-            <li><Link to="/annual-attraction" className="hover:text-[#819BE2]">Annuall<br />Attraction</Link></li>
+            <li><Link href="/Resource+Hub" className="hover:text-[#819BE2]">Resource Hub</Link></li>
+            <li><Link href="" className="hover:text-[#819BE2]">Messages</Link></li>
+            <li><Link to="/annual-attraction" className="hover:text-[#819BE2]">Annual<br />Attraction</Link></li>
             
           </ul>
         </div>
@@ -62,10 +62,10 @@ const Footer = () => {
         <div className="text-center tablet:text-left tablet:mr-[5vw] xl:mr-none">
           <h2 className="mobile:hidden text-foot text-base mb-3 tablet:block">QUICK LINKS</h2>
           <ul className="mobile:absolute tablet:static top-[18.1vw] right-[15vw] space-y-2 text-right text-base font-rajdhani">
-            <li><a href="/members" className="hover:text-[#819BE2]">Members</a></li>
-            <li><a href="/photogallery" className="hover:text-[#819BE2]">Gallery</a></li>
-            <li><a href="/about" className="hover:text-[#819BE2]">About Us</a></li>
-            <li><a href="/faq" className="hover:text-[#819BE2]">FAQs</a></li>
+            <li><Link href="/members" className="hover:text-[#819BE2]">Members</Link></li>
+            <li><Link href="/photogallery" className="hover:text-[#819BE2]">Gallery</Link></li>
+            <li><Link href="/about" className="hover:text-[#819BE2]">About Us</Link></li>
+            <li><Link href="/faq" className="hover:text-[#819BE2]">FAQs</Link></li>
           </ul>
         </div>
       </div>
